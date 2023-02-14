032be2e499be3b7af60db0dc0a3fac45f4ec0821
Chandan-DK <chandandk468@gmail.com>
2023-02-14 23:25:13 +0530

    Remove GameStartedNotifyScript
    Start methods are called before all Update methods.
    The variable hasGameStarted is set to true in the Start method.
    This causes the if statement in the Update method of TimerScript
    to always evaluate to true, which makes it useless

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -6,17 +6,9 @@ public class TimerScript : MonoBehaviour
 
     public int increaseSpeedAfterSec;
 
-    private GameStartedNotifyScript gameStartedNotifyScript;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        gameStartedNotifyScript = GameObject.FindObjectOfType<GameStartedNotifyScript>();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if (gameStartedNotifyScript.hasGameStarted) timeElapsed += Time.deltaTime;
+        timeElapsed += Time.deltaTime;
     }
 }
