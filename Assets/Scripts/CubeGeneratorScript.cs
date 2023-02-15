21c2d927ca9127f3498be6b3ebe741aeb3c4b2fc
Chandan-DK <chandandk468@gmail.com>
2023-02-15 14:18:12 +0530

    Rename CubeGenerateScript to CubeGeneratorScript

--- Assets/Scripts/CubeGeneratorScript.cs
+++ Assets/Scripts/CubeGeneratorScript.cs
@@ -2,7 +2,7 @@ using System;
 using TMPro;
 using UnityEngine;
 
-public class CubeGenerateScript : MonoBehaviour
+public class CubeGeneratorScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject cube;
